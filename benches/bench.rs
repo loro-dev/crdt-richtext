e5b8b81b2aedde2dba15574477b24059603b5f83
Zixuan Chen <remch183@outlook.com>
2023-02-28 17:42:23 +0800

    bench: add flamegraph bench to bench directly

--- benches/bench.rs
+++ benches/bench.rs
@@ -3,8 +3,11 @@ use rand::{Rng, SeedableRng};
 
 #[cfg(feature = "test")]
 pub fn bench(c: &mut Criterion) {
+    use std::fs::File;
+
     use arbitrary::Unstructured;
     use crdt_range::test_utils::{fuzzing, Action};
+    use pprof::flamegraph::{Direction, Options};
     let mut data = rand::rngs::StdRng::seed_from_u64(0);
     let mut bytes: Vec<u8> = Vec::new();
     for _ in 0..10000 {
@@ -13,15 +16,24 @@ pub fn bench(c: &mut Criterion) {
 
     let mut u = Unstructured::new(&bytes);
     let actions: [Action; 1000] = u.arbitrary().unwrap();
-    println!("actions: {:?}", actions.len());
     let actions = actions.to_vec();
-    c.bench_function("fuzzing 5", |b| {
+    let mut b = c.benchmark_group("fuzz");
+    b.sample_size(10);
+    let guard = pprof::ProfilerGuard::new(100).unwrap();
+    b.bench_function("5 actors 1000 actions", |b| {
         b.iter(|| fuzzing(5, actions.clone()));
     });
+    if let Ok(report) = guard.report().build() {
+        let file = File::create("target/flamegraph.svg").unwrap();
+        let mut options = Options::default();
+        options.direction = Direction::Inverted;
+        report.flamegraph_with_options(file, &mut options).unwrap();
+    };
 }
 
 #[cfg(not(feature = "test"))]
 pub fn bench(c: &mut Criterion) {}
 
 criterion_group!(benches, bench);
+
 criterion_main!(benches);
