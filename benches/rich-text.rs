bf3f6dd163df0729897a34d83b686ed54633805d
Zixuan Chen <remch183@outlook.com>
2023-05-05 11:29:40 +0800

    bench: refine bench cmd

--- benches/rich-text.rs
+++ benches/rich-text.rs
@@ -4,7 +4,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 mod automerge;
 
 pub fn bench(c: &mut Criterion) {
-    c.bench_function("automerge", |b| {
+    c.bench_function("rich-text automerge", |b| {
         let actions = get_automerge_actions();
         b.iter(|| {
             let mut text = RichText::new(1);
@@ -19,18 +19,18 @@ pub fn bench(c: &mut Criterion) {
         })
     });
 
-    let actions = get_automerge_actions();
-    let mut a = RichText::new(1);
-    for action in actions.iter() {
-        if action.del > 0 {
-            a.delete(action.pos..action.pos + action.del);
-        }
-        if !action.ins.is_empty() {
-            a.insert(action.pos, &action.ins);
-        }
-    }
-
     c.bench_function("automerge apply", |bench| {
+        let actions = get_automerge_actions();
+        let mut a = RichText::new(1);
+        for action in actions.iter() {
+            if action.del > 0 {
+                a.delete(action.pos..action.pos + action.del);
+            }
+            if !action.ins.is_empty() {
+                a.insert(action.pos, &action.ins);
+            }
+        }
+
         bench.iter(|| {
             let mut b = RichText::new(1);
             b.merge(&a);
