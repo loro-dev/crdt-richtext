fe5892fa482369a0727ddc512e96b151f838d299
Zixuan Chen <remch183@outlook.com>
2023-05-06 10:18:44 +0800

    test: harder bold test

--- crdt-richtext-wasm/tests/rich_text.test.ts
+++ crdt-richtext-wasm/tests/rich_text.test.ts
@@ -14,16 +14,17 @@ describe("rich-text", () => {
     const text = new RichText(BigInt(2));
     text.insert(0, "123");
     text.annotate(0, 1, "bold", AnnotateType.BoldLike);
+    text.insert(1, "k");
     {
       const spans = text.getAnnSpans();
-      expect(spans[0].text).toBe("1");
+      expect(spans[0].text).toBe("1k");
       expect(spans[0].annotations).toStrictEqual(new Set(["bold"]));
     }
 
-    text.eraseAnn(0, 1, "bold", AnnotateType.BoldLike);
+    text.eraseAnn(0, 2, "bold", AnnotateType.BoldLike);
     {
       const spans = text.getAnnSpans();
-      expect(spans[0].text).toBe("123");
+      expect(spans[0].text).toBe("1k23");
       expect(spans[0].annotations.size).toBe(0);
     }
   });
