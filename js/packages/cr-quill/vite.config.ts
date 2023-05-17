386d804d0ff1f693ce104614d6a0df5a66b74a32
Zixuan Chen <remch183@outlook.com>
2023-05-17 16:40:30 +0800

    build: fix demo build

--- js/packages/cr-quill/vite.config.ts
+++ js/packages/cr-quill/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), wasm()],
+  plugins: [vue(), wasm(), topLevelAwait()],
 });
